f34d79e95bcb6462828f768a0a9bab818c3f928e
Kamil <kgoralczyk@jash.pl>
2023-03-16 14:30:27 +0100

    Init

---
+++ productpricelabel.php
@@ -0,0 +1,48 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Productpricelabel extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'productpricelabel';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Kamil';
+        $this->need_instance = 0;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Product price label');
+        $this->description = $this->l('Displays label if product price is under 100 PLN.');
+        $this->ps_versions_compliancy = ['min' => '1.7.8.0', 'max' => _PS_VERSION_];
+    }
+
+    public function install()
+    {
+        return parent::install() && $this->registerHook('actionProductFlagsModifier');
+    }
+
+    public function hookActionProductFlagsModifier(array $params)
+    {
+        if (!isset($params['flags']) || !isset($params['product']['id_product'])) {
+            return;
+        }
+
+        $idProduct = (int)$params['product']['id_product'];
+        $productObject = new Product($idProduct);
+        $productPrice = $productObject->getPrice();
+
+        if ($productPrice < 100) {
+            $params['flags']['under_100'] = [
+                'type' => 'new',
+                'label' => $this->l('Best online price'),
+            ];
+        }
+
+        return $params;
+    }
+}
---
+++ translations/pl.php
@@ -0,0 +1,7 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{productpricelabel}prestashop>productpricelabel_a0829d6081514a9ee97f5afdec6125fa'] = 'Etykieta ceny produktu';
+$_MODULE['<{productpricelabel}prestashop>productpricelabel_25e1529a42f10c897508d2171de116de'] = 'Wyświetla etykietę jeśli cena produktu jest mniejsza niż 100 zł.';
+$_MODULE['<{productpricelabel}prestashop>productpricelabel_687ce84141fccdbab1381c4474210064'] = 'Najlepsza cena w sieci';
